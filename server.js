2bb5cbf4cf21fa3153a414f7db393e1eb5c2e780
jcprice12 <jcprice12@gmail.com>
2017-08-31 17:50:43 -0400

    using environment variables for server.js

--- server.js
+++ server.js
@@ -6,12 +6,12 @@ var mysql = require("mysql");
 
 //variables
 var app = express();
-var PORT = 8080;
+var PORT = process.env.PORT || 8080;
 var connection = mysql.createConnection({
     host: "localhost",
     port: 3306,
-    user: "root",
-    password: "root",
+    user: process.env.MYSQL_USER,
+    password: process.env.MYSQL_PASSWORD,
     database: "friends_finder_db"
 });
 
